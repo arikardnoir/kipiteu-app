14817974732f4ce4dc69a9b933f43dcc40ea9733
Kimuano Luis <kymwanuluis@gmail.com>
2024-05-27 15:10:50 +0100

    Tela de Subscrição: permitir que um clique na checkbox mude a cor das borda; Aumento dos cards e melhoramento do codigo

--- lib/screens/account/subscription_screen.dart
+++ lib/screens/account/subscription_screen.dart
@@ -28,184 +28,46 @@ class _SubscriptionScreenState extends State<SubscriptionScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 12.0),
-              padding: const EdgeInsets.all(8.0),
-              decoration: BoxDecoration(
-                color: Colors.transparent,
-                borderRadius: BorderRadius.circular(7),
-                border: Border.all(
-                  color: Colors.grey,
-                ),
-              ),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Grátis',
-                          style: TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.w500),
-                        ),
-                        Text(
-                          'Tens receitas para três dias',
-                          style: TextStyle(
-                            fontSize: 14,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        _isCheckedFree = !_isCheckedFree;
-                      });
-                    },
-                    child: Container(
-                      height: 24,
-                      width: 24,
-                      decoration: BoxDecoration(
-                        color: _isCheckedFree ? Colors.redAccent : Colors.grey,
-                        borderRadius: BorderRadius.circular(12),
-                        border: Border.all(
-                          color: Colors.grey,
-                        ),
-                      ),
-                      child: _isCheckedFree
-                          ? const Icon(
-                              Icons.check,
-                              color: Colors.white,
-                              size: 16,
-                            )
-                          : null,
-                    ),
-                  ),
-                ],
-              ),
+            _buildCard(
+              title: 'Grátis',
+              description: 'Tens receitas para três dias',
+              price: '',
+              isChecked: _isCheckedFree,
+              onTap: () {
+                setState(() {
+                  _isCheckedFree = true;
+                  _isCheckedStandard = false;
+                  _isCheckedPremium = false;
+                });
+              },
             ),
             const SizedBox(height: 20),
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 12.0),
-              padding: const EdgeInsets.all(8.0),
-              decoration: BoxDecoration(
-                color: Colors.transparent,
-                borderRadius: BorderRadius.circular(7),
-                border: Border.all(color: Colors.grey),
-              ),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Padrão',
-                          style: TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.w500),
-                        ),
-                        Text(
-                          'Tens receitas para cinco dias',
-                          style: TextStyle(
-                            fontSize: 14,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        _isCheckedStandard = !_isCheckedStandard;
-                      });
-                    },
-                    child: Container(
-                      height: 24,
-                      width: 24,
-                      decoration: BoxDecoration(
-                        color: _isCheckedStandard
-                            ? Colors.redAccent
-                            : Colors.transparent,
-                        borderRadius: BorderRadius.circular(12),
-                        border: Border.all(
-                          color: Colors.grey,
-                        ),
-                      ),
-                      child: _isCheckedStandard
-                          ? const Icon(
-                              Icons.check,
-                              color: Colors.white,
-                              size: 16,
-                            )
-                          : null,
-                    ),
-                  ),
-                ],
-              ),
+            _buildCard(
+              title: 'Padrão',
+              description: 'Tens receitas para cinco dias',
+              price: '10 R\$ por Mês',
+              isChecked: _isCheckedStandard,
+              onTap: () {
+                setState(() {
+                  _isCheckedFree = false;
+                  _isCheckedStandard = true;
+                  _isCheckedPremium = false;
+                });
+              },
             ),
             const SizedBox(height: 20),
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 12.0),
-              padding: const EdgeInsets.all(8.0),
-              decoration: BoxDecoration(
-                color: Colors.transparent,
-                borderRadius: BorderRadius.circular(7),
-                border: Border.all(color: Colors.grey),
-              ),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Premium',
-                          style: TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.w500),
-                        ),
-                        Text(
-                          'Tens receitas para sete dias',
-                          style: TextStyle(
-                            fontSize: 14,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        _isCheckedPremium = !_isCheckedPremium;
-                      });
-                    },
-                    child: Container(
-                      height: 24,
-                      width: 24,
-                      decoration: BoxDecoration(
-                        color: _isCheckedPremium
-                            ? Colors.redAccent
-                            : Colors.transparent,
-                        borderRadius: BorderRadius.circular(12),
-                        border: Border.all(
-                          color: Colors.grey,
-                        ),
-                      ),
-                      child: _isCheckedPremium
-                          ? const Icon(
-                              Icons.check,
-                              color: Colors.white,
-                              size: 16,
-                            )
-                          : null,
-                    ),
-                  ),
-                ],
-              ),
+            _buildCard(
+              title: 'Premium',
+              description: 'Tens receitas para sete dias',
+              price: '15 R\$ por Mês',
+              isChecked: _isCheckedPremium,
+              onTap: () {
+                setState(() {
+                  _isCheckedFree = false;
+                  _isCheckedStandard = false;
+                  _isCheckedPremium = true;
+                });
+              },
             ),
             const SizedBox(
                 height: 300), // Space between the card and the button
@@ -232,6 +94,80 @@ class _SubscriptionScreenState extends State<SubscriptionScreen> {
                   'Continuar',
                   style: TextStyle(fontSize: 16.0),
                 ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCard({
+    required String title,
+    required String description,
+    required String price,
+    required bool isChecked,
+    required VoidCallback onTap,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 12.0),
+        padding: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(7),
+          border: Border.all(
+            color: isChecked ? Colors.redAccent : Colors.grey,
+          ),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: const TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.w500),
+                  ),
+                  Text(
+                    description,
+                    style: const TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                  if (price.isNotEmpty) const SizedBox(height: 8),
+                  if (price.isNotEmpty)
+                    Text(
+                      price,
+                      style: const TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.w500),
+                    ),
+                ],
+              ),
+            ),
+            GestureDetector(
+              onTap: onTap,
+              child: Container(
+                height: 24,
+                width: 24,
+                decoration: BoxDecoration(
+                  color: isChecked ? Colors.redAccent : Colors.transparent,
+                  borderRadius: BorderRadius.circular(12),
+                  border: Border.all(
+                    color: isChecked ? Colors.redAccent : Colors.grey,
+                  ),
+                ),
+                child: isChecked
+                    ? const Icon(
+                        Icons.check,
+                        color: Colors.white,
+                        size: 16,
+                      )
+                    : null,
               ),
             ),
           ],
